708262368d001a259c36e7fc2a68440db5da915d
Yoan <yoandiny14@gmail.com>
2025-07-24 09:32:00 +0300

    feat: answered Q3 and Q4

--- main.py
+++ main.py
@@ -15,3 +15,26 @@ def welcome(name: str):
     return JSONResponse(content={"message": "Welcome "+ name}, status_code=200)
 
     
+class Student(BaseModel):
+    Reference: str
+    FirstName: str
+    LastName: str
+    Age: int
+
+
+student_list = []
+
+@app.post('/students')
+
+def students(student: Student):
+    
+    student_list.append(student)
+    
+    return student_list
+    
+
+    
+
+@app.get('/students')
+def get_student():
+    return student_list

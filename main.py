79ffa7f47b7d787cf137e22b4aa5a884633fad6b
Yoan <yoandiny14@gmail.com>
2025-07-24 09:17:00 +0300

    feat: added answer for Q1, Q2

--- main.py
+++ main.py
@@ -4,23 +4,14 @@ from starlette.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
-
-class User(BaseModel):
-    username: str
-    password: str
 
 
-@app.post('/login')
-def login(user: User):
-    if(user.username == "admin" and user.password == "password"):
-        return JSONResponse(content={"message": "Login successful"}, status_code=200)
-    else:
-        return JSONResponse(content={"message": "Login failed"}, status_code=401)
+@app.get("/hello")
+def hello():
+    return JSONResponse(content={"message": "Hello World"}, status_code=200)
+
+@app.get('/welcome')
+def welcome(name: str):
+    return JSONResponse(content={"message": "Welcome "+ name}, status_code=200)
+
     
